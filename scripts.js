fea2eec0432ff1c96fabe87e1fd7235c1540ce84
HallRB <Richardbrianhall@gmail.com>
2020-09-21 15:48:43 -0500

    level 1 started, cover art, dialogue functions

--- scripts.js
+++ scripts.js
@@ -3,4 +3,9 @@ function StartGame () {
     document.getElementById("welcome").style.visibility = "visible";
     document.getElementById("startbutton").style.visibility="hidden";
     console.log('success');
+} 
+
+function tavernEnter () {
+    document.getElementsByTagName("h2").style.visibility="visible";
+    document.getElementsByTagName("h1").style.visibility="hidden";
 }
