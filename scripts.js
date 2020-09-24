96de453a9f8c86bdbaa32cc98fc443c13faa3398
HallRB <Richardbrianhall@gmail.com>
2020-09-23 20:18:22 -0500

    battle simulation complete

--- scripts.js
+++ scripts.js
@@ -2,10 +2,24 @@
 function StartGame () {
     document.getElementById("welcome").style.visibility = "visible";
     document.getElementById("startbutton").style.visibility="hidden";
+    document.getElementById("characterselectionbuttons").style.visibility="visible";
     console.log('success');
 } 
 
-function tavernEnter () {
-    document.getElementsByTagName("h2").style.visibility="visible";
-    document.getElementsByTagName("h1").style.visibility="hidden";
+//character selection
+
+
+function WizardSelect () {
+    document.getElementById("characterselectionbuttons").style.visibility = "hidden";
+    document.getElementById("characterselectionname").style.visibility = "visible";
+}
+
+function KnightSelect () {
+    document.getElementById("characterselectionbuttons").style.visibility = "hidden";
+    document.getElementById("characterselectionname").style.visibility = "visible";
+}
+
+function RangerSelect () {
+    document.getElementById("characterselectionbuttons").style.visibility = "hidden";
+    document.getElementById("characterselectionname").style.visibility = "visible";
 }
